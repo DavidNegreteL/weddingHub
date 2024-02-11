46bb764a0e1a4055c8d7638957bf96d69f2d8cc6
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2024-02-10 19:33:43 -0600

    feature/back: Migration to App Router

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Montserrat } from 'next/font/google';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import '@/styles/globals.css';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="es" className={montserrat.className}>
+			<body>
+				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
+					{children}+{' '}
+				</AppRouterCacheProvider>
+			</body>
+		</html>
+	);
+}
