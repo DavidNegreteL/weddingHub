cbff4de3d99bd77f167799a956ba21d7bf5f678e
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2024-02-10 19:46:11 -0600

    theme provider added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+'use client';
 import { Montserrat } from 'next/font/google';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import '@/styles/globals.css';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@src/theme/weddingTheme';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 export default function RootLayout({
@@ -12,7 +15,7 @@ export default function RootLayout({
 		<html lang="es" className={montserrat.className}>
 			<body>
 				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
-					{children}+{' '}
+					<ThemeProvider theme={theme}>{children}+ </ThemeProvider>
 				</AppRouterCacheProvider>
 			</body>
 		</html>
