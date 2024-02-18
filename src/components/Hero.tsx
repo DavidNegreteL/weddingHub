4484e4fcbbb5f82cec8c1406565139c1034d4e1c
DAVID NEGRETE LOPEZ <dnegretel@MacBook-Air-de-DAVID.local>
2024-02-18 17:23:48 -0600

    feature/ui: Img configStyles prop added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import ResponsiveImage from "./ResponsiveImage";
+import DecorativeImage from "./DecorativeImage";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
@@ -84,26 +86,44 @@ function Hero() {
           palabra fin.
         </Typography>
       </Container>
-      <Image
-        src={"/images/hero.jpeg"}
-        alt="ara-y-lalo-1"
+      <ResponsiveImage
+        imageSrc={"/images/hero.jpeg"}
+        altText="ara-y-lalo-1"
         width={360}
         height={480}
-        className="rounded-tl-lg rounded-br-lg ml-0 duration-300 md:ml-auto"
+        configStyles={{
+          borderTopLeftRadius: "80px",
+          borderBottomRightRadius: "80px",
+        }}
       />
-      <Image
-        src={"/images/hero_decoration_left_2.png"}
-        alt="decoracion izquierda"
+      <DecorativeImage
+        imageSrc={"/images/hero_decoration_left_2.png"}
+        altText="decoracion izquierda"
         width={360}
         height={480}
-        className="absolute -bottom-l right-8 md:top-72 right-24"
+        configStyles={{
+          position: "absolute",
+          bottom: "-40px",
+          right: "32px",
+          [theme.breakpoints.up("sm")]: {
+            top: "288px",
+            right: "96px",
+          },
+        }}
       />
-      <Image
-        src={"/images/hero_decoration_right.png"}
-        alt="decoracion derecha"
+      <DecorativeImage
+        imageSrc={"/images/hero_decoration_right.png"}
+        altText="decoracion derecha"
         width={360}
         height={480}
-        className="absolute -top-44 -right-8 md:-right-28"
+        configStyles={{
+          position: "absolute",
+          top: "-176px",
+          right: "-32px",
+          [theme.breakpoints.up("sm")]: {
+            right: "-112px",
+          },
+        }}
       />
     </Container>
   );
